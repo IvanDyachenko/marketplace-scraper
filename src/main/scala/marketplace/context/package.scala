ce25de111977a0b831d78fbd7d459984e5bfe35a
Ivan Dyachenko <vandyachen@gmail.com>
2020-11-17 19:34:33 +0300

    Use monix.Task instead of cats.IO Use tofu.Env instead of tofu.ContexT Remove F[_] from CrawlerContext Implement:   + ClickhouseXa   + doobie looging based on tofu
    Useful links:
    https://github.com/TinkoffCreditSystems/tofu/blob/master/doobie/src/test/scala/tofu/doobie/example/TofuDoobieExample.scala

--- src/main/scala/marketplace/context/package.scala
+++ src/main/scala/marketplace/context/package.scala
@@ -1,11 +1,12 @@
 package marketplace
 
 import tofu.WithContext
-import tofu.concurrent.ContextT
+import tofu.env.Env
+
 import marketplace.config.CrawlerConfig
 
 package object context {
-  type CrawlerF[F[+_], +A] = ContextT[F, CrawlerContext, A]
+  type CrawlerF[+A] = Env[CrawlerContext, A]
 
   type HasConfig[F[_]] = F WithContext CrawlerConfig
 }

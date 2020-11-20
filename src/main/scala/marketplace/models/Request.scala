207347ca420f631ee75febbf438457b09c26bd0a
Ivan Dyachenko <vandyachen@gmail.com>
2020-11-20 19:10:50 +0300

    Implementation of HttpClient based on http4s is added

--- src/main/scala/marketplace/models/Request.scala
+++ src/main/scala/marketplace/models/Request.scala
@@ -1,3 +1,12 @@
 package marketplace.models
 
-sealed trait Request
+import derevo.derive
+import tofu.logging.derivation.loggable
+import io.circe.Encoder
+
+@derive(loggable)
+final case class Request(uri: String, headers: Headers, path: Path, queryParams: Map[String, Seq[String]])
+
+object Request {
+  implicit val circeEncoder: Encoder[Request] = ???
+}

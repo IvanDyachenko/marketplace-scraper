0a30e3350df919fbe63f5c3e2f97dfeb7a47e69a
Ivan Dyachenko <vandyachen@gmail.com>
2020-12-11 01:40:53 +0300

    Clean up

--- src/main/scala/marketplace/clients/models/HttpRequest.scala
+++ src/main/scala/marketplace/clients/models/HttpRequest.scala
@@ -1,8 +1,6 @@
 package marketplace.clients.models
 
 import cats.implicits._
-import cats.Inject
-import vulcan.Codec
 import io.circe.syntax._
 import io.circe.Encoder
 import tofu.logging.{DictLoggable, LogRenderer, Loggable}
@@ -16,8 +14,7 @@ trait HttpRequest[+R] {
 }
 
 object HttpRequest {
-  implicit def circeEncoder[R: Encoder]: Encoder[HttpRequest[R]]             = Encoder.instance(_.body.asJson)
-  implicit def vulcanCodec[R: Inject[*, Array[Byte]]]: Codec[HttpRequest[R]] = ???
+  implicit def circeEncoder[R: Encoder]: Encoder[HttpRequest[R]] = Encoder.instance(_.body.asJson)
 
   implicit def loggable[Req]: Loggable[HttpRequest[Req]] = new DictLoggable[HttpRequest[Req]] {
     def logShow(a: HttpRequest[Req]): String = s"{a.uri.host}/${a.uri.path.show}"

e74e9dfe093f846d14ba355854ae963673bc77df
Ivan Dyachenko <vandyachen@gmail.com>
2020-12-18 02:26:35 +0300

    Add AvroSettings[App] to Environment

--- src/main/scala/marketplace/env/package.scala
+++ src/main/scala/marketplace/env/package.scala
@@ -1,5 +1,6 @@
 package marketplace
 
+import cats.effect.ConcurrentEffect
 import tofu.WithContext
 import tofu.env.Env
 
@@ -8,5 +9,6 @@ import marketplace.config.Config
 package object env {
   type App[+A] = Env[Environment, A]
 
+  type HasCE[F[_]]     = F WithContext ConcurrentEffect[F]
   type HasConfig[F[_]] = F WithContext Config
 }

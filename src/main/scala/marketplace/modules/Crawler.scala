e7491fd41f6aa149307e02526781526d5b3910ed
Ivan Dyachenko <vandyachen@gmail.com>
2020-12-21 02:27:30 +0300

    Crawler implementation based on AppI effect instead of AppF is added Remove config-field from AppContext

--- src/main/scala/marketplace/modules/Crawler.scala
+++ src/main/scala/marketplace/modules/Crawler.scala
@@ -1,31 +1,63 @@
 package marketplace.modules
 
-import cats.effect.Resource
+import cats.Monad
+import cats.effect.{Concurrent, Resource, Timer}
+import cats.tagless.syntax.functorK._
 import tofu.WithRun
+import tofu.syntax.embed._
+import tofu.syntax.monadic._
+import tofu.syntax.context._
 import derevo.derive
 import tofu.higherKind.derived.representableK
-import fs2.kafka.{KafkaConsumer, KafkaProducer}
+import fs2.Stream
+import tofu.fs2.LiftStream
+import fs2.kafka.{commitBatchWithin, KafkaConsumer, KafkaProducer, ProducerRecord, ProducerRecords}
 
 import marketplace.config.CrawlerConfig
 import marketplace.context.AppContext
 import marketplace.services.Crawl
-import marketplace.models.{CommandId, EventId}
+import marketplace.models.{CommandKey, EventKey}
 import marketplace.models.crawler.{Command, Event}
 
 @derive(representableK)
-trait Crawler[F[_]] {
-  def run: F[Unit]
+trait Crawler[S[_]] {
+  def run: S[Unit]
 }
 
 object Crawler {
 
   def apply[F[_]](implicit ev: Crawler[F]): ev.type = ev
 
-  def make[I[_], F[_]: WithRun[*[_], I, AppContext]](
+  def make[I[_]: Monad: Concurrent: Timer, F[_]: WithRun[*[_], I, AppContext], S[_]: LiftStream[*[_], I]](
     crawl: Crawl[F],
-    consumer: KafkaConsumer[I, CommandId, Command],
-    producer: KafkaProducer[I, EventId, Event]
-  )(
-    crawlerConfig: CrawlerConfig
-  ): Resource[I, Crawler[I]] = ???
+    consumer: KafkaConsumer[I, CommandKey, Command],
+    producer: KafkaProducer[I, EventKey, Event]
+  )(config: CrawlerConfig): Resource[I, Crawler[S]] =
+    Resource.liftF {
+      Stream
+        .eval {
+          val impl: Crawler[Stream[I, *]] = new Crawler[Stream[I, *]] {
+            def run: Stream[I, Unit] =
+              consumer.partitionedStream.map { partition =>
+                partition
+                  .evalMap { committable =>
+                    runContext(crawl.handle(committable.record.value))(AppContext()).map { event =>
+                      ProducerRecords.one(
+                        ProducerRecord(config.eventsTopic, event.key, event),
+                        committable.offset
+                      )
+                    }
+                  }
+                  .through(_.evalMap(producer.produce).mapAsync(1000)(identity))
+                  .map(_.passthrough)
+                  .through(commitBatchWithin(config.batchOffsets, config.batchTimeWindow))
+              }.parJoinUnbounded
+          }
+
+          impl.pure[I]
+        }
+        .embed
+        .mapK(LiftStream[S, I].liftF)
+        .pure[I]
+    }
 }

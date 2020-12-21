8ca0b51515615c29f2f82ff1df0e5c6f83e6e84b
Ivan Dyachenko <vandyachen@gmail.com>
2020-12-22 02:07:38 +0300

    Update Parser configuration

--- src/main/scala/marketplace/config/ParserConfig.scala
+++ src/main/scala/marketplace/config/ParserConfig.scala
@@ -1,11 +1,19 @@
 package marketplace.config
 
+import scala.concurrent.duration.FiniteDuration
+
 import cats.effect.{Blocker, ContextShift, Sync}
 import pureconfig.ConfigSource
 import pureconfig.generic.auto._
 import pureconfig.module.catseffect.syntax._
 
-final case class ParserConfig(maxOpen: Int, maxConcurrent: Int)
+final case class ParserConfig(
+  groupId: String,
+  eventsTopic: String,
+  commandsTopic: String,
+  batchOffsets: Int,
+  batchTimeWindow: FiniteDuration
+)
 
 object ParserConfig {
   lazy val load: ParserConfig = ConfigSource.default.at("parser").loadOrThrow[ParserConfig]

2c496a085a8b02f1602a12afd0a1b1e656cd4a12
Ivan Dyachenko <vandyachen@gmail.com>
2020-12-23 01:01:53 +0300

    Scheduler now produce messages to Kafka

--- src/main/scala/marketplace/modules/Scheduler.scala
+++ src/main/scala/marketplace/modules/Scheduler.scala
@@ -1,14 +1,15 @@
 package marketplace.modules
 
 import cats.implicits._
-import cats.{Foldable, Monad}
-import cats.effect.{Resource, Timer}
+import cats.Monad
+import cats.effect.{Concurrent, Resource, Timer}
 import cats.tagless.syntax.functorK._
 import tofu.syntax.embed._
 import derevo.derive
 import tofu.higherKind.derived.representableK
 import fs2.Stream
 import tofu.fs2.LiftStream
+import fs2.kafka.{KafkaProducer, ProducerRecord, ProducerRecords}
 
 import marketplace.config.SchedulerConfig
 
@@ -21,21 +22,29 @@ object Scheduler {
 
   def apply[F[_]](implicit ev: Scheduler[F]): ev.type = ev
 
-  def make[I[_]: Monad: Timer, S[_]: LiftStream[*[_], I], O, G[_]: Foldable](
-    f: => G[O]
-  )(config: SchedulerConfig): Resource[I, Scheduler[S]] =
+  def make[F[_]: Monad: Timer: Concurrent, S[_]: LiftStream[*[_], F], K, V](producer: KafkaProducer[F, K, V])(
+    config: SchedulerConfig,
+    topic: String
+  )(f: Stream[F, (K, V)]): Resource[F, Scheduler[S]] =
     Resource.liftF {
       Stream
         .eval {
-          val impl: Scheduler[Stream[I, *]] = new Scheduler[Stream[I, *]] {
-            def run: Stream[I, Unit] =
-              Stream.awakeEvery[I](config.timeout).zipRight(Stream.emits(f.toList)).as(())
+          val impl: Scheduler[Stream[F, *]] = new Scheduler[Stream[F, *]] {
+
+            def run: Stream[F, Unit] =
+              Stream
+                .awakeEvery[F](config.timeout)
+                .zipRight(f)
+                .map { case (k, v) => ProducerRecords.one(ProducerRecord(topic, k, v)) }
+                .through(_.evalMap(producer.produce[Unit]).mapAsync(1000)(identity))
+                .map(_.passthrough)
+                .repeat
           }
 
-          impl.pure[I]
+          impl.pure[F]
         }
         .embed
-        .mapK(LiftStream[S, I].liftF)
-        .pure[I]
+        .mapK(LiftStream[S, F].liftF)
+        .pure[F]
     }
 }

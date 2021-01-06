2e03f98846d44bb9d150af035282c764da51ff54
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-01-06 00:57:13 +0000

    Update doobie-core, doobie-hikari to 0.10.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -11,7 +11,7 @@ object Versions {
   lazy val fs2Kafka        = "1.1.0+101-ee43a77b-SNAPSHOT"
   lazy val http4s          = "0.21.15"
   lazy val asyncHttpClient = "2.12.2"
-  lazy val doobie          = "0.9.4"
+  lazy val doobie          = "0.10.0"
   lazy val clickhouseJDBC  = "0.2.4"
   lazy val pureconfig      = "0.14.0"
   lazy val logback         = "1.2.3"

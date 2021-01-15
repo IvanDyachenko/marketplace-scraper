26764bc1f0622856bfbad503f1d683c798af6c4c
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-01-15 15:48:32 +0300

    Template.State.UniversalAction (which contains quantity and maxItems) is added to marketplace.models.ozon

--- src/main/scala/marketplace/models/ozon/Component.scala
+++ src/main/scala/marketplace/models/ozon/Component.scala
@@ -7,6 +7,7 @@ import io.circe.generic.extras.Configuration
 import io.circe.generic.extras.semiauto.deriveConfiguredDecoder
 import supertagged.TaggedType
 
+import marketplace.syntax._
 import marketplace.models.{LiftedCats, LiftedCirce, LiftedLoggable, LiftedVulcanCodec}
 
 @derive(loggable)
@@ -24,10 +25,6 @@ object Component {
   @derive(loggable)
   final case class SearchResultsV2(stateId: Component.StateId) extends Component
 
-  implicit val circeDecoderConfig: Configuration = Configuration(Predef.identity, decapitalize, false, Some("component"))
+  implicit val circeDecoderConfig: Configuration = Configuration(Predef.identity, _.decapitalize, false, Some("component"))
   implicit val circeDecoder: Decoder[Component]  = deriveConfiguredDecoder[Component]
-
-  private def decapitalize(s: String): String =
-    if (s == null || s.length == 0 || s.charAt(0).isLower) s
-    else s.updated(0, s.charAt(0).toLower)
 }

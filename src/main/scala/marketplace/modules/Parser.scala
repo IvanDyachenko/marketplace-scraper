32a59749650b03a00f33214e9224f4ed25ec43c5
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-01-18 17:16:49 +0300

    Use ToFu streams to abstract functional streaming effects in a TF way https://docs.tofu.tf/docs/streams

--- src/main/scala/marketplace/modules/Parser.scala
+++ src/main/scala/marketplace/modules/Parser.scala
@@ -1,16 +1,16 @@
 package marketplace.modules
 
+import cats.tagless.syntax.functorK._
 import cats.Monad
 import cats.effect.{Concurrent, Resource, Timer}
-import cats.tagless.syntax.functorK._
-import tofu.WithRun
 import tofu.syntax.embed._
 import tofu.syntax.monadic._
 import tofu.syntax.context._
 import derevo.derive
 import tofu.higherKind.derived.representableK
-import fs2.Stream
+import tofu.WithRun
 import tofu.fs2.LiftStream
+import fs2.Stream
 import fs2.kafka.{commitBatchWithin, KafkaConsumer, KafkaProducer, ProducerRecord, ProducerRecords}
 
 import marketplace.config.ParserConfig
@@ -25,31 +25,36 @@ trait Parser[S[_]] {
 }
 
 object Parser {
-
   def apply[F[_]](implicit ev: Parser[F]): ev.type = ev
+
+  private final class Impl[I[_]: Monad: Timer: Concurrent, F[_]: WithRun[*[_], I, AppContext]](config: ParserConfig)(
+    parse: Parse[F],
+    producerOfParserEvents: KafkaProducer[I, Event.Key, ParserEvent],
+    consumerOfParserCommands: KafkaConsumer[I, Command.Key, ParserCommand]
+  ) extends Parser[Stream[I, *]] {
+    def run: Stream[I, Unit] =
+      consumerOfParserCommands.partitionedStream.map { partition =>
+        partition
+          .parEvalMap(config.maxConcurrent) { committable =>
+            runContext(parse.handle(committable.record.value))(AppContext()).map(_ -> committable.offset)
+          }
+          .map { case (event, offset) => ProducerRecords.one(ProducerRecord(config.eventsTopic, event.key, event), offset) }
+          .evalMap(producerOfParserEvents.produce)
+          .parEvalMap(config.maxConcurrent)(identity)
+          .map(_.passthrough)
+          .through(commitBatchWithin(config.batchOffsets, config.batchTimeWindow))
+      }.parJoinUnbounded
+  }
 
   def make[I[_]: Monad: Concurrent: Timer, F[_]: WithRun[*[_], I, AppContext], S[_]: LiftStream[*[_], I]](config: ParserConfig)(
     parse: Parse[F],
-    consumer: KafkaConsumer[I, Command.Key, ParserCommand],
-    producer: KafkaProducer[I, Event.Key, ParserEvent]
+    producerOfParserEvents: KafkaProducer[I, Event.Key, ParserEvent],
+    consumerOfParserCommands: KafkaConsumer[I, Command.Key, ParserCommand]
   ): Resource[I, Parser[S]] =
     Resource.liftF {
       Stream
         .eval {
-          val impl: Parser[Stream[I, *]] = new Parser[Stream[I, *]] {
-            def run: Stream[I, Unit] =
-              consumer.partitionedStream.map { partition =>
-                partition
-                  .parEvalMap(config.maxConcurrent) { committable =>
-                    runContext(parse.handle(committable.record.value))(AppContext()).map(_ -> committable.offset)
-                  }
-                  .map { case (event, offset) => ProducerRecords.one(ProducerRecord(config.eventsTopic, event.key, event), offset) }
-                  .evalMap(producer.produce)
-                  .parEvalMap(1000)(identity)
-                  .map(_.passthrough)
-                  .through(commitBatchWithin(config.batchOffsets, config.batchTimeWindow))
-              }.parJoinUnbounded
-          }
+          val impl: Parser[Stream[I, *]] = new Impl[I, F](config)(parse, producerOfParserEvents, consumerOfParserCommands)
 
           impl.pure[I]
         }

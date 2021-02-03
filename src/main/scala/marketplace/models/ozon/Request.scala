6f0c1105a3ce3929375bb696c0348ecd97ad4a44
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-02-03 15:45:15 +0300

    GetCategory model is added to marketplace.models.ozon.Request

--- src/main/scala/marketplace/models/ozon/Request.scala
+++ src/main/scala/marketplace/models/ozon/Request.scala
@@ -17,35 +17,45 @@ sealed trait Request {
   def url: Url
 }
 
-@derive(loggable)
-final case class GetCategorySearchResultsV2(
-  categoryName: Category.Name,
-  page: Page,
-  layoutContainer: LayoutContainer = LayoutContainer.Default,
-  layoutPageIndex: LayoutPageIndex
-) extends Request {
-  val url = Url(s"/category/${categoryName.show}/", Some(page), Some(layoutContainer), Some(layoutPageIndex))
-}
-
-object GetCategorySearchResultsV2 {
-  def apply(categoryName: Category.Name, page: Page): GetCategorySearchResultsV2 =
-    GetCategorySearchResultsV2(categoryName, page, LayoutContainer.Default, page.self @@ LayoutPageIndex)
-
-  implicit val vulcanCodec: Codec[GetCategorySearchResultsV2] =
-    Codec.record[GetCategorySearchResultsV2](
-      name = "GetCategorySearchResultsV2",
-      namespace = "ozon.models"
-    ) { field =>
-      field("host", _.host) *> field("path", _.path) *> field("url", _.url) *>
-        (
-          field("categoryName", _.categoryName),
-          field("page", _.page),
-          field("layoutContainer", _.layoutContainer),
-          field("layoutPageIndex", _.layoutPageIndex)
-        ).mapN(apply)
-    }
-}
-
 object Request {
-  implicit val vulcanCodec: Codec[Request] = Codec.union[Request](alt => alt[GetCategorySearchResultsV2])
+
+  @derive(loggable)
+  final case class GetCategory(categoryId: Category.Id) extends Request {
+    val url = Url(s"/modal/categoryMenu/category/${categoryId.show}/")
+  }
+
+  @derive(loggable)
+  final case class GetCategorySearchResultsV2(
+    categoryName: Category.Name,
+    page: Page,
+    layoutContainer: LayoutContainer = LayoutContainer.Default,
+    layoutPageIndex: LayoutPageIndex
+  ) extends Request {
+    val url = Url(s"/category/${categoryName.show}/", Some(page), Some(layoutContainer), Some(layoutPageIndex))
+  }
+
+  object GetCategory {
+    implicit val vulcanCodec: Codec[GetCategory] =
+      Codec.record[GetCategory](name = "GetCategory", namespace = "ozon.models")(field =>
+        field("host", _.host) *> field("path", _.path) *> field("url", _.url) *> field("categoryId", _.categoryId).map(apply)
+      )
+  }
+
+  object GetCategorySearchResultsV2 {
+    def apply(categoryName: Category.Name, page: Page): GetCategorySearchResultsV2 =
+      GetCategorySearchResultsV2(categoryName, page, LayoutContainer.Default, page.self @@ LayoutPageIndex)
+
+    implicit val vulcanCodec: Codec[GetCategorySearchResultsV2] =
+      Codec.record[GetCategorySearchResultsV2](name = "GetCategorySearchResultsV2", namespace = "ozon.models") { field =>
+        field("host", _.host) *> field("path", _.path) *> field("url", _.url) *>
+          (
+            field("categoryName", _.categoryName),
+            field("page", _.page),
+            field("layoutContainer", _.layoutContainer),
+            field("layoutPageIndex", _.layoutPageIndex)
+          ).mapN(apply)
+      }
+  }
+
+  implicit val vulcanCodec: Codec[Request] = Codec.union[Request](alt => alt[GetCategory] |+| alt[GetCategorySearchResultsV2])
 }

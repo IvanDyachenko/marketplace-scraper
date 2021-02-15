0798800844d86f5a7ece92c310e926c6b1e2ef07
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-02-15 00:38:38 +0000

    Update fs2-kafka, fs2-kafka-vulcan to 1.1.0+103-aa8842e2-SNAPSHOT

--- project/Versions.scala
+++ project/Versions.scala
@@ -8,7 +8,7 @@ object Versions {
   lazy val circeDerivation = "0.13.0-M5"
   lazy val vulkan          = "1.3.0"
   lazy val fs2             = "2.5.0"
-  lazy val fs2Kafka        = "1.1.0+101-ee43a77b-SNAPSHOT"
+  lazy val fs2Kafka        = "1.1.0+103-aa8842e2-SNAPSHOT"
   lazy val http4s          = "0.21.19"
   lazy val asyncHttpClient = "2.12.2"
   lazy val doobie          = "0.10.0"

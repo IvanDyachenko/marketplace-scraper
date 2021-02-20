da2cba879cfdbc837c7eb6fefc8b445c4510ec40
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-02-21 01:34:53 +0300

    Support ozon.Item which cannot be shipped (availability is equal to 5) (#148)

--- src/main/scala/marketplace/models/ozon/items/InStock.scala
+++ src/main/scala/marketplace/models/ozon/items/InStock.scala
@@ -8,7 +8,7 @@ import io.circe.{Decoder, DecodingFailure, HCursor}
 import marketplace.models.ozon.{Brand, Category, Delivery, Item, MarketplaceSeller, Price, Rating, Template}
 
 @derive(loggable)
-final case class InStock(
+final case class InStock private (
   id: Item.Id,
   index: Int,
   `type`: Item.Type,
@@ -18,6 +18,7 @@ final case class InStock(
   rating: Rating,
   categoryPath: Category.Path,
   delivery: Delivery,
+  availability: Short,
   availableInDays: Short,
   marketplaceSellerId: MarketplaceSeller.Id,
   addToCartMinItems: Int,
@@ -28,46 +29,45 @@ final case class InStock(
   isPersonalized: Boolean,
   isPromotedProduct: Boolean,
   freeRest: Int
-) extends Item {
-  val availability: Item.Availability = Item.Availability.InStock
-}
+) extends Item
 
 object InStock {
   implicit val circeDecoder: Decoder[InStock] = Decoder.instance[InStock] { (c: HCursor) =>
     lazy val i = c.downField("cellTrackingInfo")
 
     for {
-      _        <- i.get[Item.Availability]("availability")
-                    .ensure {
-                      val message = s"'cellTrackingInfo' doesn't contain 'availability' which is equal to '${Item.Availability.InStock.value}'"
-                      DecodingFailure(message, c.history)
-                    }(_ == Item.Availability.InStock)
-      addToCart = c.get[Template]("templateState")
-                    .flatMap(_.addToCart.fold[Decoder.Result[(Int, Int)]] {
-                      val message = "'templateState' doesn't contain an object which describes 'addToCart...' action"
-                      Left(DecodingFailure(message, c.history))
-                    }(Right(_)))
-      item     <- (
-                    i.get[Item.Id]("id"),
-                    i.get[Int]("index"),
-                    i.get[Item.Type]("type"),
-                    i.get[String]("title"),
-                    i.as[Brand],
-                    i.as[Price],
-                    i.as[Rating],
-                    i.get[Category.Path]("category"),
-                    i.as[Delivery],
-                    i.get[Short]("availableInDays"),
-                    i.get[MarketplaceSeller.Id]("marketplaceSellerId"),
-                    addToCart.map(_._1),
-                    addToCart.map(_._2),
-                    c.get[Boolean]("isAdult"),
-                    c.get[Boolean]("isAlcohol"),
-                    i.get[Boolean]("isSupermarket"),
-                    i.get[Boolean]("isPersonalized"),
-                    i.get[Boolean]("isPromotedProduct"),
-                    i.get[Int]("freeRest")
-                  ).mapN(apply)
+      availability <- i.get[Short]("availability")
+                        .ensure {
+                          val message = s"'cellTrackingInfo' doesn't contain 'availability' which is equal to '${Item.Availability.InStock}'"
+                          DecodingFailure(message, c.history)
+                        }(Item.Availability.from(_) == Item.Availability.InStock)
+      addToCart     = c.get[Template]("templateState")
+                        .flatMap(_.addToCart.fold[Decoder.Result[(Int, Int)]] {
+                          val message = "'templateState' doesn't contain an object which describes 'addToCart...' action"
+                          Left(DecodingFailure(message, c.history))
+                        }(Right(_)))
+      item         <- (
+                        i.get[Item.Id]("id"),
+                        i.get[Int]("index"),
+                        i.get[Item.Type]("type"),
+                        i.get[String]("title"),
+                        i.as[Brand],
+                        i.as[Price],
+                        i.as[Rating],
+                        i.get[Category.Path]("category"),
+                        i.as[Delivery],
+                        Right(availability),
+                        i.get[Short]("availableInDays"),
+                        i.get[MarketplaceSeller.Id]("marketplaceSellerId"),
+                        addToCart.map(_._1),
+                        addToCart.map(_._2),
+                        c.get[Boolean]("isAdult"),
+                        c.get[Boolean]("isAlcohol"),
+                        i.get[Boolean]("isSupermarket"),
+                        i.get[Boolean]("isPersonalized"),
+                        i.get[Boolean]("isPromotedProduct"),
+                        i.get[Int]("freeRest")
+                      ).mapN(apply)
     } yield item
   }
 }

c41a9aac4eb26d70002772fb4f54b8b603e17721
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-03-01 00:41:39 +0000

    Update pureconfig, pureconfig-cats-effect, ... to 0.14.1

--- project/Versions.scala
+++ project/Versions.scala
@@ -15,7 +15,7 @@ object Versions {
   lazy val asyncHttpClient       = "2.12.2"
   lazy val doobie                = "0.10.0"
   lazy val clickhouseJDBC        = "0.2.6"
-  lazy val pureconfig            = "0.14.0"
+  lazy val pureconfig            = "0.14.1"
   lazy val logback               = "1.2.3"
 
   lazy val betterMonadicFor = "0.3.1"

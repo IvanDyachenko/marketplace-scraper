7f254476de63e708737908f7097a48530b34e681
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-03-02 17:30:53 +0300

    Add "http4s-jdk-http-client" library to build dependencies

--- src/main/scala/marketplace/clients/HttpClient.scala
+++ src/main/scala/marketplace/clients/HttpClient.scala
@@ -2,7 +2,6 @@ package marketplace.clients
 
 import java.util.concurrent.TimeoutException
 import scala.util.control.NoStackTrace
-import scala.concurrent.duration._
 
 import cats.syntax.show._
 import tofu.syntax.raise._
@@ -10,21 +9,17 @@ import tofu.syntax.handle._
 import tofu.syntax.monadic._
 import derevo.derive
 import tofu.logging.derivation.loggable
-import cats.{FlatMap, Monad}
-import cats.effect.{ConcurrentEffect, Resource, Sync, Timer}
-import tofu.{Execute, Handle, Raise}
+import cats.FlatMap
+import cats.effect.{ConcurrentEffect, Resource, Sync}
+import tofu.{Handle, Raise}
 import tofu.lift.Unlift
 import tofu.higherKind.Embed
 import tofu.data.derived.ContextEmbed
-import tofu.logging.{Logging, Logs}
+import tofu.logging.Logs
 import io.circe.Decoder
 import org.http4s.{DecodeFailure, Request => Http4sRequest, Status}
 import org.http4s.circe.jsonOf
 import org.http4s.client.{Client, ConnectionFailure}
-import org.http4s.client.middleware.{GZip, Retry, RetryPolicy}
-import org.http4s.client.blaze.BlazeClientBuilder
-//import org.asynchttpclient.Dsl
-//import org.http4s.client.asynchttpclient.AsyncHttpClient
 
 import marketplace.config.HttpConfig
 
@@ -55,8 +50,7 @@ object HttpClient extends ContextEmbed[HttpClient] {
   type Raising[F[_]]  = Raise[F, HttpClientError]
   type Handling[F[_]] = Handle[F, HttpClientError]
 
-  class Impl[F[_]: Sync: Logging: Raising: Handle[*[_], TimeoutException]: Handle[*[_], DecodeFailure]](http4sClient: Client[F])
-      extends HttpClient[F] {
+  class Impl[F[_]: Sync: Handle[*[_], TimeoutException]: Handle[*[_], DecodeFailure]](http4sClient: Client[F]) extends HttpClient[F] {
 
     def send[Res](request: Http4sRequest[F])(implicit decoder: Decoder[Res]): F[Res] =
       http4sClient
@@ -104,16 +98,18 @@ object HttpClient extends ContextEmbed[HttpClient] {
   def apply[F[_]](implicit ev: HttpClient[F]): ev.type = ev
 
   def make[
-    I[_]: Monad: Timer: Execute: ConcurrentEffect: Unlift[*[_], F],
-    F[_]: Sync: Raising
+    I[_]: ConcurrentEffect: Unlift[*[_], F],
+    F[_]: Sync
   ](httpConfig: HttpConfig)(implicit logs: Logs[I, F]): Resource[I, HttpClient[F]] =
     buildHttp4sClient[I](httpConfig) >>= { http4sClient =>
-      Resource.liftF(logs.forService[HttpClient[F]].map(implicit l => new Impl[F](translateHttp4sClient[I, F](http4sClient))))
+      Resource.liftF(logs.forService[HttpClient[F]].map(_ => new Impl[F](translateHttp4sClient[I, F](http4sClient))))
     }
 
   // https://scastie.scala-lang.org/Odomontois/F29lLrY2RReZrcUJ1zIEEg/25
-  private def translateHttp4sClient[F[_]: Sync, G[_]: Sync](client: Client[F])(implicit U: Unlift[F, G]): Client[G] =
+  private def translateHttp4sClient[F[_], G[_]: Sync](client: Client[F])(implicit U: Unlift[F, G]): Client[G] =
     Client(req => Resource.suspend(U.unlift.map(gf => client.run(req.mapK(gf)).mapK(U.liftF).map(_.mapK(U.liftF)))))
+
+  private def buildHttp4sClient[F[_]: ConcurrentEffect](httpConfig: HttpConfig): Resource[F, Client[F]] = ???
 
   // private def buildHttp4sClient[F[_]: ConcurrentEffect](httpConfig: HttpConfig): Resource[F, Client[F]] =
   //   AsyncHttpClient.resource {
@@ -127,23 +123,23 @@ object HttpClient extends ContextEmbed[HttpClient] {
   //       .build()
   //   }
 
-  private def buildHttp4sClient[F[_]: Timer: Execute: ConcurrentEffect](httpConfig: HttpConfig): Resource[F, Client[F]] =
-    Resource.liftF(Execute[F].executionContext) >>= (
-      BlazeClientBuilder[F](_)
-        .withTcpNoDelay(true) // Disable Nagle's algorithm.
-        .withSocketKeepAlive(true)
-        .withCheckEndpointAuthentication(false)
-        .withMaxTotalConnections(httpConfig.maxTotalConnections)
-        .withMaxConnectionsPerRequestKey(Function.const(httpConfig.maxTotalConnectionsPerHost))
-        .withMaxWaitQueueLimit(httpConfig.maxWaitQueueLimit)
-        .withIdleTimeout(httpConfig.idleTimeout)
-        .withConnectTimeout(httpConfig.connectTimeout)
-        .withRequestTimeout(httpConfig.requestTimeout)
-        .resource
-        .map(client => GZip()(client))
-        .map(client => Retry(recklesslyRetryPolicy(httpConfig.requestMaxDelayBetweenAttempts, httpConfig.requestMaxTotalAttempts))(client))
-    )
+  // private def buildHttp4sClient[F[_]: Timer: Execute: ConcurrentEffect](httpConfig: HttpConfig): Resource[F, Client[F]] =
+  //   Resource.liftF(Execute[F].executionContext) >>= (
+  //     BlazeClientBuilder[F](_)
+  //       .withTcpNoDelay(true) // Disable Nagle's algorithm.
+  //       .withSocketKeepAlive(true)
+  //       .withCheckEndpointAuthentication(false)
+  //       .withMaxTotalConnections(httpConfig.maxTotalConnections)
+  //       .withMaxConnectionsPerRequestKey(Function.const(httpConfig.maxTotalConnectionsPerHost))
+  //       .withMaxWaitQueueLimit(httpConfig.maxWaitQueueLimit)
+  //       .withIdleTimeout(httpConfig.idleTimeout)
+  //       .withConnectTimeout(httpConfig.connectTimeout)
+  //       .withRequestTimeout(httpConfig.requestTimeout)
+  //       .resource
+  //       .map(client => GZip()(client))
+  //       .map(client => Retry(recklesslyRetryPolicy(httpConfig.requestMaxDelayBetweenAttempts, httpConfig.requestMaxTotalAttempts))(client))
+  //   )
 
-  private def recklesslyRetryPolicy[F[_]](maxWait: Duration, maxRetry: Int): RetryPolicy[F] =
-    RetryPolicy(RetryPolicy.exponentialBackoff(maxWait = maxWait, maxRetry = maxRetry), (_, result) => RetryPolicy.recklesslyRetriable(result))
+  // private def recklesslyRetryPolicy[F[_]](maxWait: Duration, maxRetry: Int): RetryPolicy[F] =
+  //   RetryPolicy(RetryPolicy.exponentialBackoff(maxWait = maxWait, maxRetry = maxRetry), (_, result) => RetryPolicy.recklesslyRetriable(result))
 }

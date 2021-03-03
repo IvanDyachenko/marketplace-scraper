3988e2b21f96bb737ee4c72ca316dff4404e38d1
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-03-03 22:03:24 +0300

    Set correct Avro namespace for ozon.Delivery.Schema, ozon.Item.Type and ozon.Type.Availability Clean up

--- src/main/scala/net/dalytics/models/ozon/Delivery.scala
+++ src/main/scala/net/dalytics/models/ozon/Delivery.scala
@@ -8,6 +8,7 @@ import tofu.logging.LoggableEnum
 import enumeratum.{CatsEnum, CirceEnum, Enum, EnumEntry, VulcanEnum}
 import enumeratum.EnumEntry.{Camelcase, Uppercase}
 import vulcan.Codec
+import vulcan.generic.AvroNamespace
 import io.circe.Decoder
 
 @derive(loggable)
@@ -15,6 +16,7 @@ final case class Delivery(schema: Delivery.Schema, timeDiffDays: Short)
 
 object Delivery {
 
+  @AvroNamespace("ozon.models.delivery")
   sealed trait Schema extends EnumEntry with Camelcase with Product with Serializable
   object Schema       extends Enum[Schema] with CatsEnum[Schema] with CirceEnum[Schema] with LoggableEnum[Schema] with VulcanEnum[Schema] {
 

cef45b0a11840c07b09369e0d1cb6982e3cb7f3e
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-03-07 22:07:16 +0300

    Add logging to OzonApi and WildBerriesApi (#257)

--- modules/domain/src/main/scala/net/dalytics/api/WildBerriesApi.scala
+++ modules/domain/src/main/scala/net/dalytics/api/WildBerriesApi.scala
@@ -1,14 +1,18 @@
 package net.dalytics.api
 
-import cats.{~>, Functor}
+import tofu.syntax.raise._
 import tofu.syntax.handle._
 import tofu.syntax.monadic._
+import tofu.syntax.logging._
+import cats.{~>, Functor, Monad}
+import cats.effect.Resource
+import tofu.logging.{Logging, Logs}
 import fs2.Stream
 import tofu.lift.Lift
 import tofu.fs2.LiftStream
 
 import net.dalytics.marshalling._
-import net.dalytics.clients.HttpClient
+import net.dalytics.clients.{HttpClient, HttpClientError}
 import net.dalytics.models.wildberries.{Catalog, CatalogMenu, Request}
 
 trait WildBerriesApi[F[_], S[_]] {
@@ -17,20 +21,36 @@ trait WildBerriesApi[F[_], S[_]] {
 }
 
 object WildBerriesApi {
-  final class Impl[F[_]: Functor: HttpClient: HttpClient.Handling] extends WildBerriesApi[F, Stream[F, *]] {
+
+  final class Impl[F[_]: Monad: Logging: HttpClient: HttpClient.Raising: HttpClient.Handling] extends WildBerriesApi[F, Stream[F, *]] {
+
     def getCatalog(id: Catalog.Id): F[Option[Catalog]] = getCatalogMenu.map(_ >>= (_.catalog(id)))
 
     def getCatalogs(rootId: Catalog.Id)(p: Catalog => Boolean): Stream[F, Catalog] =
       Stream.eval(getCatalog(rootId)) >>= (_.fold[Stream[F, Catalog]](Stream.empty)(catalog => Stream.emits(catalog.filter(p))))
 
-    private def getCatalogMenu: F[Option[CatalogMenu]] = HttpClient[F].send[CatalogMenu](Request.GetCatalogMenu).restore
+    private def getCatalogMenu: F[Option[CatalogMenu]] = {
+      val request = Request.GetCatalogMenu
+
+      HttpClient[F]
+        .send[CatalogMenu](request)
+        .recoverWith[HttpClientError](error =>
+          errorCause"Error was thrown while attempting to execute ${request}" (error) *> error.raise[F, CatalogMenu]
+        )
+        .restore
+    }
   }
 
   def make[
-    F[_]: Functor: HttpClient: HttpClient.Handling,
+    I[_]: Monad,
+    F[_]: Monad: HttpClient: HttpClient.Raising: HttpClient.Handling,
     S[_]: LiftStream[*[_], F]
-  ]: WildBerriesApi[F, S] =
-    bifunctorK.bimapK(new Impl[F])(Lift.liftIdentity[F].liftF)(LiftStream[S, F].liftF)
+  ](implicit logs: Logs[I, F]): Resource[I, WildBerriesApi[F, S]] =
+    Resource.liftF {
+      logs
+        .forService[WildBerriesApi[F, S]]
+        .map(implicit l => bifunctorK.bimapK(new Impl[F])(Lift.liftIdentity[F].liftF)(LiftStream[S, F].liftF))
+    }
 
   implicit val bifunctorK: BifunctorK[WildBerriesApi] =
     new BifunctorK[WildBerriesApi] {

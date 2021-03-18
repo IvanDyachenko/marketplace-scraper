54b2a04276759bffff9e4477a00a95d5272e4145
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-03-18 20:30:23 +0300

    Sale.aggregate method is implememented. This method calculcate number of sold items accroding to https://gitlab.com/dalytics/analytics/-/issues/26

--- modules/domain/src/main/scala/net/dalytics/models/ozon/items/OutOfStock.scala
+++ modules/domain/src/main/scala/net/dalytics/models/ozon/items/OutOfStock.scala
@@ -26,7 +26,7 @@ final case class OutOfStock private (
   isPromotedProduct: Boolean,
   freeRest: Int
 ) extends Item {
-  val addToCart: Item.AddToCart = Item.AddToCart.Unavailable
+  val addToCart: Item.AddToCart = Item.AddToCart.With(0, 0)
 }
 
 object OutOfStock {

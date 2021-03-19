775bcba8aca8693558ffeca4543d2680bd3d946f
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-03-19 03:52:15 +0300

    Enricher module is implemented. Add KafkaStreamsConfig which contains some useful configuration parameters for Kafka Stream application. Clean up

---
+++ modules/enricher/src/main/scala/net/dalytics/App.scala
@@ -0,0 +1,22 @@
+package net.dalytics
+
+import monix.eval.{Task, TaskApp}
+import cats.effect.{Blocker, ExitCode, Resource}
+import fs2.kafka.vulcan.SchemaRegistryClientSettings
+
+import net.dalytics.config.Config
+
+object Main extends TaskApp {
+
+  override def run(args: List[String]): Task[ExitCode] = init.use(_.run.executeAsync).as(ExitCode.Success)
+
+  type AppI[+A] = Task[A]
+
+  private def init: Resource[Task, Enricher[AppI]] =
+    for {
+      implicit0(blocker: Blocker) <- Blocker[AppI]
+      cfg                         <- Resource.liftF(Config.make[AppI])
+      schemaRegistryClient        <- Resource.liftF(SchemaRegistryClientSettings[AppI](cfg.schemaRegistryConfig.baseUrl).createSchemaRegistryClient)
+      aggregator                  <- Enricher.make[AppI](cfg)(schemaRegistryClient)
+    } yield aggregator
+}
---
+++ modules/enricher/src/main/scala/net/dalytics/serde/VulcanSerdeCodec.scala
@@ -0,0 +1,48 @@
+package net.dalytics.serde
+
+import compstak.kafkastreams4s.{Codec => KafkaStreams4sCodec}
+import org.apache.kafka.common.serialization.Serde
+import vulcan.{Codec => VulcanCodec}
+import org.apache.kafka.common.serialization.Serializer
+import org.apache.kafka.common.serialization.Deserializer
+
+trait VulcanSerdeCodec[A] {
+  def serde: Serde[A]
+  def vulcanCodec: VulcanCodec[A]
+}
+
+object VulcanSerdeCodec {
+
+  implicit val kafkaStreams4sCodec: KafkaStreams4sCodec[VulcanSerdeCodec] =
+    new KafkaStreams4sCodec[VulcanSerdeCodec] { self =>
+      def serde[A](implicit vulcanSerdeCodec: VulcanSerdeCodec[A]): Serde[A] = vulcanSerdeCodec.serde
+
+      def optionSerde[A](implicit vulcanSerdeCodec: VulcanSerdeCodec[A]): VulcanSerdeCodec[Option[A]] =
+        new VulcanSerdeCodec[Option[A]] {
+          def serde: Serde[Option[A]] = new Serde[Option[A]] {
+            def serializer(): Serializer[Option[A]]     = new Serializer[Option[A]] {
+              def serialize(topic: String, data: Option[A]): Array[Byte] =
+                data match {
+                  case Some(data) => self.serde.serializer.serialize(topic, data)
+                  case None       => null
+                }
+            }
+            def deserializer(): Deserializer[Option[A]] = new Deserializer[Option[A]] {
+              def deserialize(topic: String, bytes: Array[Byte]): Option[A] =
+                if (bytes == null)
+                  None
+                else
+                  Some(self.serde.deserializer.deserialize(topic, bytes))
+            }
+          }
+
+          def vulcanCodec: VulcanCodec[Option[A]] = VulcanCodec.option[A](vulcanSerdeCodec.vulcanCodec)
+        }
+    }
+
+  implicit def vulcanSerdeCodec[A](implicit _serde: Serde[A], _vulcanCodec: VulcanCodec[A]): VulcanSerdeCodec[A] =
+    new VulcanSerdeCodec[A] {
+      def serde: Serde[A]             = _serde
+      def vulcanCodec: VulcanCodec[A] = _vulcanCodec
+    }
+}

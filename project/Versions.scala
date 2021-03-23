ed382ffa97fe4c1a740e135152cee2c0a0f4daa7
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-03-23 00:41:51 +0000

    Update kafka-streams to 6.1.1-ce

--- project/Versions.scala
+++ project/Versions.scala
@@ -9,7 +9,7 @@ object Versions {
   lazy val vulkan                = "1.3.0"
   lazy val fs2                   = "2.5.3"
   lazy val fs2Kafka              = "1.4.1"
-  lazy val kafkaStreams          = "2.7.0"
+  lazy val kafkaStreams          = "6.1.1-ce"
   lazy val kafkaStreams4s        = "0.15.0"
   lazy val kafkaStreamsAvroSerde = "6.1.0"
   lazy val http4s                = "0.21.20"

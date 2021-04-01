ce57bb7cee753a92b79782302aa56fc66fffec0d
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-04-01 00:17:02 +0000

    Update fs2-kafka, fs2-kafka-vulcan to 1.4.2

--- project/Versions.scala
+++ project/Versions.scala
@@ -8,7 +8,7 @@ object Versions {
   lazy val circeDerivation        = "0.13.0-M5"
   lazy val vulkan                 = "1.3.0"
   lazy val fs2                    = "2.5.3"
-  lazy val fs2Kafka               = "1.4.1"
+  lazy val fs2Kafka               = "1.4.2"
   lazy val kafkaStreams           = "6.1.1-ce"
   lazy val kafkaStreams4s         = "0.15.0"
   lazy val kafkaStreamsAvroSerde  = "6.1.1"

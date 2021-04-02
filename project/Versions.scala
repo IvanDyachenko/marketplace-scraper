3fea0e3886ffee8a3d96757ea514ea22a1d411d2
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-04-02 00:23:48 +0000

    Update scalatest, scalatest-flatspec to 3.2.7

--- project/Versions.scala
+++ project/Versions.scala
@@ -28,7 +28,7 @@ object Versions {
   lazy val kindProjector    = "0.11.3"
 
   lazy val scalactic               = "3.2.6"
-  lazy val scalatest               = "3.2.6"
+  lazy val scalatest               = "3.2.7"
   lazy val scalacheck              = "1.15.3"
   lazy val scalatestPlusScalacheck = "3.2.2.0"
 }

c649a3e8ad2896456b4bd0e84eafb3186e7f7587
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-04-07 19:53:05 +0300

    Clean up

--- modules/enricher/src/main/scala/net/dalytics/Enricher.scala
+++ modules/enricher/src/main/scala/net/dalytics/Enricher.scala
@@ -5,17 +5,19 @@ import java.util.Properties
 
 import tofu.syntax.monadic._
 import cats.effect.{Concurrent, Resource, Sync}
+import org.apache.kafka.common.serialization.Serde
 import org.apache.kafka.streams.StreamsBuilder
-import org.apache.kafka.streams.kstream.{Consumed, Grouped, Materialized, Produced, ValueMapper}
+import org.apache.kafka.streams.kstream.Produced
 import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient
 import fs2.kafka.vulcan.AvroSettings
 import compstak.kafkastreams4s.Platform
 
 import net.dalytics.config.Config
-import net.dalytics.models.{ozon, Event}
+import net.dalytics.models.Event
 import net.dalytics.models.parser.ParserEvent
 import net.dalytics.models.enricher.EnricherEvent
 import net.dalytics.clients.KafkaClient
+import net.dalytics.wrappers.vulcan.KStream4sVulcan
 import net.dalytics.serdes.VulcanSerde
 import net.dalytics.extractors.EventTimestampExtractor
 
@@ -28,52 +30,45 @@ object Enricher {
 
   private final class Impl[F[_]: Concurrent](cfg: Config)(schemaRegistryClient: SchemaRegistryClient) extends Enricher[F] {
     def run: F[Unit] = {
-      val avroSettings: AvroSettings[F]    = KafkaClient.makeAvroSettings[F](schemaRegistryClient)
-      val streamsConfiguration: Properties = KafkaClient.makeKafkaStreamsConfiguration[EventTimestampExtractor](
+      val avroSettings: AvroSettings[F]  = KafkaClient.makeAvroSettings[F](schemaRegistryClient)
+      val streamsConfig: Properties      = KafkaClient.makeKafkaStreamsConfiguration[EventTimestampExtractor](
         cfg.kafkaConfig,
         cfg.schemaRegistryConfig,
         cfg.kafkaStreamsConfig
       )
-      val streamsBuilder: StreamsBuilder   = new StreamsBuilder
+      val streamsTimeout: Duration       = Duration.ofNanos(cfg.kafkaStreamsConfig.closeTimeout.toNanos)
+      val streamsBuilder: StreamsBuilder = new StreamsBuilder
 
       for {
-        eventKeySerde      <- VulcanSerde[Event.Key].using(avroSettings)(true)
-        parserEventSerde   <- VulcanSerde[ParserEvent].using(avroSettings)(false)
-        enricherEventSerde <- VulcanSerde[EnricherEvent].using(avroSettings)(false)
-        parserEventsKStream = streamsBuilder.stream(cfg.kafkaStreamsConfig.sourceTopic, Consumed.`with`(eventKeySerde, parserEventSerde))
-        _                  <- Sync[F].delay {
-                                parserEventsKStream
-                                  .filter { (_: Event.Key, event: ParserEvent) =>
-                                    event match {
-                                      case _: ParserEvent.OzonCategorySearchResultsV2ItemParsed => true
-                                      case _                                                    => false
-                                    }
-                                  }
-                                  .mapValues({ (event: ParserEvent) =>
-                                    val ParserEvent.OzonCategorySearchResultsV2ItemParsed(created, timestamp, page, item, category) = event
-                                    EnricherEvent.OzonCategorySearchResultsV2ItemEnriched(created, timestamp, page, item, ozon.Sale.Unknown, category)
-                                  }: ValueMapper[ParserEvent, EnricherEvent])
-                                  .groupByKey(Grouped.`with`(eventKeySerde, enricherEventSerde))
-                                  .reduce(
-                                    (prevEvent: EnricherEvent, currEvent: EnricherEvent) => {
-                                      val _prevEvent = prevEvent.asInstanceOf[EnricherEvent.OzonCategorySearchResultsV2ItemEnriched]
-                                      val _currEvent = currEvent.asInstanceOf[EnricherEvent.OzonCategorySearchResultsV2ItemEnriched]
-                                      _prevEvent.aggregate(_currEvent)
-                                    },
-                                    Materialized.`with`(eventKeySerde, enricherEventSerde)
-                                  )
-                                  .toStream((key: Event.Key, event: EnricherEvent) => event.key.getOrElse(key))
-                                  .to(cfg.kafkaStreamsConfig.sinkTopic, Produced.`with`[Event.Key, EnricherEvent](eventKeySerde, enricherEventSerde))
-                              }
-        topology            = streamsBuilder.build()
-        platform           <- Platform.run[F](topology, streamsConfiguration, Duration.ofNanos(cfg.kafkaStreamsConfig.closeTimeout.toNanos)).void
+        implicit0(eventKeySerde: Serde[Event.Key])                                            <- VulcanSerde[Event.Key].using(avroSettings)(true)
+        implicit0(parserEventSerde: Serde[ParserEvent])                                       <- VulcanSerde[ParserEvent].using(avroSettings)(false)
+        implicit0(enricherEventSerde: Serde[EnricherEvent.OzonCategoryResultsV2ItemEnriched]) <-
+          VulcanSerde[EnricherEvent.OzonCategoryResultsV2ItemEnriched].using(avroSettings)(false)
+
+        _ <- Sync[F].delay {
+               KStream4sVulcan[Event.Key, ParserEvent](streamsBuilder, cfg.kafkaStreamsConfig.sourceTopic)
+                 .collect {
+                   case ParserEvent.OzonCategorySearchResultsV2ItemParsed(created, timestamp, page, item, category)  =>
+                     EnricherEvent.OzonCategoryResultsV2ItemEnriched(created, timestamp, page, item, category)
+                   case ParserEvent.OzonCategorySoldOutResultsV2ItemParsed(created, timestamp, page, item, category) =>
+                     EnricherEvent.OzonCategoryResultsV2ItemEnriched(created, timestamp, page, item, category)
+                 }
+                 .reduceByKey(_ aggregate _)
+                 .toKTable
+                 .toStream((_, event) => event.key.get)
+                 .to(
+                   cfg.kafkaStreamsConfig.sinkTopic,
+                   Produced.`with`[Event.Key, EnricherEvent.OzonCategoryResultsV2ItemEnriched](eventKeySerde, enricherEventSerde)
+                 )
+             }
+
+        topology  = streamsBuilder.build()
+        platform <- Platform.run[F](topology, streamsConfig, streamsTimeout).void
       } yield platform
     }
   }
 
-  def make[
-    F[_]: Concurrent
-  ](cfg: Config)(schemaRegistryClient: SchemaRegistryClient): Resource[F, Enricher[F]] =
+  def make[F[_]: Concurrent](cfg: Config)(schemaRegistryClient: SchemaRegistryClient): Resource[F, Enricher[F]] =
     Resource.eval {
       val impl = new Impl[F](cfg)(schemaRegistryClient): Enricher[F]
 

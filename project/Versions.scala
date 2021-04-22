3ea18830cc125078dba9a97bd76a7bdb4739b28a
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-04-22 23:38:56 +0300

    Tethis (a JSON parsing/writing library) is added to library dependencies

--- project/Versions.scala
+++ project/Versions.scala
@@ -8,6 +8,7 @@ object Versions {
   lazy val enumeratum             = "1.6.1"
   lazy val circe                  = "0.13.0"
   lazy val circeDerivation        = "0.13.0-M5"
+  lazy val tethys                 = "0.23.0"
   lazy val vulkan                 = "1.3.0"
   lazy val fs2                    = "2.5.5"
   lazy val fs2Kafka               = "1.6.1"

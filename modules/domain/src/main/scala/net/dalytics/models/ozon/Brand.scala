dd64c215f30a2b82d26a2d595815a300feada561
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-04-24 16:53:30 +0300

    Add JsonReader implementation for ozon.Brand model

--- modules/domain/src/main/scala/net/dalytics/models/ozon/Brand.scala
+++ modules/domain/src/main/scala/net/dalytics/models/ozon/Brand.scala
@@ -6,21 +6,23 @@ import derevo.derive
 import tofu.logging.derivation.loggable
 import vulcan.Codec
 import io.circe.Decoder
+import tethys.JsonReader
 import supertagged.TaggedType
 
-import net.dalytics.models.{LiftedCats, LiftedCirce, LiftedLoggable, LiftedVulcanCodec}
+import net.dalytics.models.{LiftedCats, LiftedCirce, LiftedLoggable, LiftedTethys, LiftedVulcanCodec}
 
 @derive(loggable)
 final case class Brand(id: Brand.Id, name: Brand.Name)
 
 object Brand {
-  object Id extends TaggedType[Long] with LiftedCats with LiftedLoggable with LiftedCirce with LiftedVulcanCodec
+  object Id extends TaggedType[Long] with LiftedCats with LiftedLoggable with LiftedCirce with LiftedTethys with LiftedVulcanCodec
   type Id = Id.Type
 
-  object Name extends TaggedType[String] with LiftedCats with LiftedLoggable with LiftedCirce with LiftedVulcanCodec
+  object Name extends TaggedType[String] with LiftedCats with LiftedLoggable with LiftedCirce with LiftedTethys with LiftedVulcanCodec
   type Name = Name.Type
 
-  implicit val circeDecoder: Decoder[Brand] = Decoder.forProduct2("brandId", "brand")(apply)
+  implicit val circeDecoder: Decoder[Brand]  = Decoder.forProduct2("brandId", "brand")(apply)
+  implicit val jsonReader: JsonReader[Brand] = JsonReader.builder.addField[Id]("brandId").addField[Name]("brand").buildReader(apply)
 
   private[models] def vulcanCodecFieldFA[A](field: Codec.FieldBuilder[A])(f: A => Brand): FreeApplicative[Codec.Field[A, *], Brand] =
     (field("brandId", f(_).id), field("brandName", f(_).name)).mapN(apply)

68b5f94de0ba128442357f3413183567346a450c
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-04-25 23:24:00 +0300

    Add JsonReader implementation for ozon.MarketplaceSeller model

--- modules/domain/src/test/scala/net/dalytics/models/ozon/MarketplaceSellerSpec.scala
+++ modules/domain/src/test/scala/net/dalytics/models/ozon/MarketplaceSellerSpec.scala
@@ -1,13 +1,18 @@
 package net.dalytics.models.ozon
 
+import org.scalatest.EitherValues
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 import io.circe.parser.decode
+import tethys._
+import tethys.jackson._
 
-class MarketplaceSellerSpec extends AnyFlatSpec with Matchers {
+import supertagged.postfix._
 
-  it should "decode MarketplaceSeller from a valid JSON" in {
+class MarketplaceSellerSpec extends AnyFlatSpec with Matchers with EitherValues {
+
+  it should "decode MarketplaceSeller from a valid JSON (circe)" in {
     val marketplaceSellerRawJson =
       """
         |{
@@ -21,41 +26,7 @@ class MarketplaceSellerSpec extends AnyFlatSpec with Matchers {
         |  "deeplink": "ozon://seller/yunistor-314/?miniapp=seller_314",
         |  "link": "/seller/yunistor-314/",
         |  "isFavorite": false,
-        |  "items": [
-        |    {
-        |      "sku": 163204456,
-        |      "title": "Ершик для унитаза UniStor",
-        |      "image": "https://cdn1.ozone.ru/s3/multimedia-b/6006833711.jpg",
-        |      "isAdult": false,
-        |      "price": 296,
-        |      "discount": 23,
-        |      "finalPrice": 227,
-        |      "link": "/seller/yunistor-314/",
-        |      "deeplink": "ozon://seller/yunistor-314/?miniapp=seller_314"
-        |    },
-        |    {
-        |      "sku": 163349018,
-        |      "title": "Ершик для унитаза UniStor",
-        |      "image": "https://cdn1.ozone.ru/s3/multimedia-7/6006830683.jpg",
-        |      "isAdult": false,
-        |      "price": 500,
-        |      "discount": 23,
-        |      "finalPrice": 384,
-        |      "link": "/seller/yunistor-314/",
-        |      "deeplink": "ozon://seller/yunistor-314/?miniapp=seller_314"
-        |    },
-        |    {
-        |      "sku": 163214590,
-        |      "title": "Сушилка для посуды UniStor , 48 см х 27 см х 11 см",
-        |      "image": "https://cdn1.ozone.ru/s3/multimedia-6/6006823950.jpg",
-        |      "isAdult": false,
-        |      "price": 1265,
-        |      "discount": 23,
-        |      "finalPrice": 973,
-        |      "link": "/seller/yunistor-314/",
-        |      "deeplink": "ozon://seller/yunistor-314/?miniapp=seller_314"
-        |    }
-        |  ],
+        |  "items": [],
         |  "utm": "",
         |  "advId": "",
         |  "trackingInfo": {
@@ -71,6 +42,43 @@ class MarketplaceSellerSpec extends AnyFlatSpec with Matchers {
         |}
       """.stripMargin
 
-    decode[MarketplaceSeller](marketplaceSellerRawJson).isRight shouldBe true
+    val marketplaceSeller = decode[MarketplaceSeller](marketplaceSellerRawJson).value
+
+    marketplaceSeller should be(MarketplaceSeller(314L @@ MarketplaceSeller.Id, "UniStor", "Taking care of your space"))
+  }
+
+  it should "decode MarketplaceSeller from a valid JSON (tethys)" in {
+    val marketplaceSellerRawJson =
+      """
+        |{
+        |  "id": 314,
+        |  "title": "UniStor",
+        |  "subtitle": "Taking care of your space",
+        |  "logoImage": "https://cdn1.ozone.ru/s3/marketing-api/banners/rE/bz/rEbzyB3nHw2V3b90lmwkkY9qfNXpLi9a.jpg",
+        |  "backgroundImage": "https://cdn1.ozone.ru/s3/marketing-api/banners/1F/5C/1F5CBuIErKeuM47SYq1kWNucnYuKYg8d.jpg",
+        |  "backgroundType": "image",
+        |  "backgroundColor": "",
+        |  "deeplink": "ozon://seller/yunistor-314/?miniapp=seller_314",
+        |  "link": "/seller/yunistor-314/",
+        |  "isFavorite": false,
+        |  "items": [],
+        |  "utm": "",
+        |  "advId": "",
+        |  "trackingInfo": {
+        |    "click": {
+        |      "actionType": "click",
+        |      "key": "6709b984f7d8ccb98c4b674409969b9ced28468f"
+        |    },
+        |    "view": {
+        |      "actionType": "view",
+        |      "key": "6709b984f7d8ccb98c4b674409969b9ced28468f"
+        |    }
+        |  }
+        |}
+      """.stripMargin
+
+    val marketplaceSeller = marketplaceSellerRawJson.jsonAs[MarketplaceSeller].value
+
+    marketplaceSeller should be(MarketplaceSeller(314L @@ MarketplaceSeller.Id, "UniStor", "Taking care of your space"))
   }
 }

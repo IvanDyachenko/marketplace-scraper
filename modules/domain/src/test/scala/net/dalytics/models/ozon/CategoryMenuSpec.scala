7a98a0e1b3a23124c8662d7375c6a60847b51e13
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-04-26 21:45:14 +0300

    Add JsonReader implementation for ozon.CategoryMenu model

--- modules/domain/src/test/scala/net/dalytics/models/ozon/CategoryMenuSpec.scala
+++ modules/domain/src/test/scala/net/dalytics/models/ozon/CategoryMenuSpec.scala
@@ -5,10 +5,14 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 import io.circe.parser.decode
+import tethys._
+import tethys.jackson._
+
+import supertagged.postfix._
 
 class CategoryMenuSpec extends AnyFlatSpec with Matchers with EitherValues with OptionValues {
 
-  it should "decode CategoryMenu from a valid JSON" in {
+  it should "decode CategoryMenu from a valid JSON (circe)" in {
     val categoryMenuRawJson =
       """
         |{
@@ -185,5 +189,90 @@ class CategoryMenuSpec extends AnyFlatSpec with Matchers with EitherValues with 
     val decodedResult = decode[Result](categoryMenuRawJson)
 
     decodedResult.value.categoryMenu.value shouldBe a[CategoryMenu]
+  }
+
+  it should "decode CategoryMenu from a valid JSON (tethys)" in {
+    val categoryMenuRawJson =
+      """
+        |{
+        |  "categoryMenu-281296-default-1": {
+        |    "categories": [
+        |      {
+        |        "id": 14500,
+        |        "name": "Дом и сад",
+        |        "isAdult": false,
+        |        "link": "/category/dom-i-sad-14500/",
+        |        "deeplink": "ozon://category/dom-i-sad-14500/",
+        |        "isActive": false,
+        |        "cellTrackingInfo": {
+        |          "type": "category",
+        |          "id": "14500",
+        |          "index": 1,
+        |          "title": "Дом и сад"
+        |        },
+        |        "categories": [
+        |          {
+        |            "id": 15078,
+        |            "name": "Текстиль",
+        |            "isAdult": false,
+        |            "link": "/category/tekstil-15078/",
+        |            "deeplink": "ozon://category/tekstil-15078/",
+        |            "isActive": false,
+        |            "cellTrackingInfo": {
+        |              "type": "category",
+        |              "id": "15078",
+        |              "index": 1,
+        |              "title": "Текстиль"
+        |            },
+        |            "categories": [
+        |              {
+        |                "id": 15085,
+        |                "name": "Постельное белье",
+        |                "isAdult": false,
+        |                "link": "/category/postelnoe-bele-15085/",
+        |                "deeplink": "ozon://category/postelnoe-bele-15085/",
+        |                "isActive": false,
+        |                "cellTrackingInfo": {
+        |                  "type": "category",
+        |                  "id": "15085",
+        |                  "index": 1,
+        |                  "title": "Постельное белье"
+        |                },
+        |                "categories": [
+        |                  {
+        |                    "id": 15086,
+        |                    "name": "Комплекты постельного белья",
+        |                    "isAdult": false,
+        |                    "link": "/category/komplekty-postelnogo-belya-15086/",
+        |                    "deeplink": "ozon://category/komplekty-postelnogo-belya-15086/",
+        |                    "isActive": true,
+        |                    "cellTrackingInfo": {
+        |                      "type": "category",
+        |                      "id": "15086",
+        |                      "index": 1,
+        |                      "title": "Комплекты постельного белья"
+        |                    },
+        |                    "categories": [],
+        |                    "redirectTo": ""
+        |                  }
+        |                ],
+        |                "redirectTo": ""
+        |              }
+        |            ],
+        |            "redirectTo": ""
+        |          }
+        |        ],
+        |        "redirectTo": ""
+        |      }
+        |    ],
+        |    "modalUrl": "/modal/categoryMenu"
+        |  }
+        |}
+      """.stripMargin
+
+    implicit val jsonReader = CategoryMenu.jsonReader(Component.CategoryMenu("categoryMenu-281296-default-1" @@ Component.StateId))
+    val categoryMenu        = categoryMenuRawJson.jsonAs[CategoryMenu].value
+
+    categoryMenu.categories.length should be(1)
   }
 }

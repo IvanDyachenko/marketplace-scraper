1ad15d70fe1ad4086d0df0728c6cc33048ebfc1b
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-04-29 00:16:28 +0000

    Update pureconfig, pureconfig-cats-effect, ... to 0.15.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -19,7 +19,7 @@ object Versions {
   lazy val http4sBlazeClient      = "0.21.22"
   lazy val doobie                 = "0.13.1"
   lazy val clickhouseJDBC         = "0.3.1"
-  lazy val pureconfig             = "0.14.1"
+  lazy val pureconfig             = "0.15.0"
 
   lazy val slf4j   = "1.7.30"
   lazy val logback = "1.2.3"

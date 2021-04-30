6a0a1cb6ba0b0216eb9859ed9e9ee05a9af4987e
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-04-30 00:08:37 +0000

    Update tofu-core, tofu-core-higher-kind, ... to 0.10.2

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,7 @@ object Versions {
   lazy val monix                  = "3.3.0"
   lazy val catsCore               = "2.6.0"
   lazy val catsEffect             = "2.5.0"
-  lazy val tofu                   = "0.10.1"
+  lazy val tofu                   = "0.10.2"
   lazy val derevo                 = "0.12.2"
   lazy val supertagged            = "2.0-RC2"
   lazy val enumeratum             = "1.6.1"

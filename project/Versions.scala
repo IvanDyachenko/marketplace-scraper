7dc47a18147beb8d8ad829b02ab8c1d16234dd66
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-07-05 00:07:37 +0000

    Update fs2-kafka, fs2-kafka-vulcan to 2.1.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -11,7 +11,7 @@ object Versions {
   lazy val tethys                 = "0.24.0"
   lazy val vulkan                 = "1.3.0"
   lazy val fs2                    = "2.5.8"
-  lazy val fs2Kafka               = "1.7.0"
+  lazy val fs2Kafka               = "2.1.0"
   lazy val kafkaStreams           = "6.2.0-ce"
   lazy val kafkaStreams4s         = "0.15.0"
   lazy val kafkaStreamsAvroSerde  = "6.2.0"

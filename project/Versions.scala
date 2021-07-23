b9827bca9ade23dc2518ec03abf85ebe404f50bd
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-07-23 23:30:49 +0300

    Update logback-classic to 1.2.4 (#492)

--- project/Versions.scala
+++ project/Versions.scala
@@ -23,7 +23,7 @@ object Versions {
   lazy val pureconfig             = "0.14.1"
 
   lazy val slf4j   = "1.7.32"
-  lazy val logback = "1.2.3"
+  lazy val logback = "1.2.4"
 
   lazy val betterMonadicFor = "0.3.1"
   lazy val kindProjector    = "0.13.0"

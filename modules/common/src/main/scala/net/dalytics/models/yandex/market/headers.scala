518980d363221c249cb30d358fb5d3197bb08eed
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-11-04 00:12:12 +0000

    Applied Scalafix rule(s) github:http4s/http4s/v0_22?sha=series/0.22

--- modules/common/src/main/scala/net/dalytics/models/yandex/market/headers.scala
+++ modules/common/src/main/scala/net/dalytics/models/yandex/market/headers.scala
@@ -4,9 +4,10 @@ package headers {
   import cats.implicits._
   import org.http4s.syntax.string._
   import org.http4s.{Header, HeaderKey, ParseFailure, ParseResult}
-  import org.http4s.util.{CaseInsensitiveString, Writer}
+  import org.http4s.util.Writer
+import org.typelevel.ci.{ CIString, _ }
 
-  final case class `X-Region-Id`(geoId: Region.GeoId) extends Header.Parsed {
+  final case class `X-Region-Id`(geoId: Region.GeoId) extends Header.Raw.Parsed {
     def key                                      = `X-Region-Id`
     def renderValue(writer: Writer): writer.type = writer.append(geoId.show)
   }
@@ -14,7 +15,7 @@ package headers {
   object `X-Region-Id` extends HeaderKey.Singleton {
     type HeaderT = `X-Region-Id`
 
-    def name: CaseInsensitiveString = "X-Region-Id".ci
+    def name: CIString = ci"X-Region-Id"
 
     def parse(s: String): ParseResult[`X-Region-Id`] =
       Region.GeoId(s) match {
@@ -22,12 +23,12 @@ package headers {
         case _           => Left(ParseFailure.apply(s, "Invalid region identifier"))
       }
 
-    def matchHeader(header: Header): Option[`X-Region-Id`] =
+    def matchHeader(header: Header.Raw): Option[`X-Region-Id`] =
       if (header.name == name) Region.GeoId(header.value).map(`X-Region-Id`(_))
       else None
   }
 
-  final case class `X-Device-Type`(deviceType: String) extends Header.Parsed {
+  final case class `X-Device-Type`(deviceType: String) extends Header.Raw.Parsed {
     def key                                      = `X-Device-Type`
     def renderValue(writer: Writer): writer.type = writer.append(deviceType)
   }
@@ -35,16 +36,16 @@ package headers {
   object `X-Device-Type` extends HeaderKey.Singleton {
     type HeaderT = `X-Device-Type`
 
-    def name: CaseInsensitiveString = "X-Device-Type".ci
+    def name: CIString = ci"X-Device-Type"
 
     def parse(s: String): ParseResult[`X-Device-Type`] = Right(`X-Device-Type`(s))
 
-    def matchHeader(header: Header): Option[`X-Device-Type`] =
+    def matchHeader(header: Header.Raw): Option[`X-Device-Type`] =
       if (header.name == name) Some(`X-Device-Type`(header.value))
       else None
   }
 
-  final case class `X-Platform`(platform: String) extends Header.Parsed {
+  final case class `X-Platform`(platform: String) extends Header.Raw.Parsed {
     def key                                      = `X-Platform`
     def renderValue(writer: Writer): writer.type = writer.append(platform)
   }
@@ -52,16 +53,16 @@ package headers {
   object `X-Platform` extends HeaderKey.Singleton {
     type HeaderT = `X-Platform`
 
-    def name: CaseInsensitiveString = "X-Platform".ci
+    def name: CIString = ci"X-Platform"
 
     def parse(s: String): ParseResult[`X-Platform`] = Right(`X-Platform`(s))
 
-    def matchHeader(header: Header): Option[`X-Platform`] =
+    def matchHeader(header: Header.Raw): Option[`X-Platform`] =
       if (header.name == name) Some(`X-Platform`(header.value))
       else None
   }
 
-  final case class `X-App-Version`(appVersion: String) extends Header.Parsed {
+  final case class `X-App-Version`(appVersion: String) extends Header.Raw.Parsed {
     def key                                      = `X-App-Version`
     def renderValue(writer: Writer): writer.type = writer.append(appVersion)
   }
@@ -69,11 +70,11 @@ package headers {
   object `X-App-Version` extends HeaderKey.Singleton {
     type HeaderT = `X-App-Version`
 
-    def name: CaseInsensitiveString = "X-App-Version".ci
+    def name: CIString = ci"X-App-Version"
 
     def parse(s: String): ParseResult[`X-App-Version`] = Right(`X-App-Version`(s))
 
-    def matchHeader(header: Header): Option[`X-App-Version`] =
+    def matchHeader(header: Header.Raw): Option[`X-App-Version`] =
       if (header.name == name) Some(`X-App-Version`(header.value))
       else None
   }

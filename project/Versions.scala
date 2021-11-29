fe3ee14613226ee0eb90dbb751cb1938004e07b0
Ivan Dyachenko <827607+IvanDyachenko@users.noreply.github.com>
2021-11-29 00:09:38 +0000

    Update cats-core to 2.7.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,6 +1,6 @@
 object Versions {
   lazy val monix                  = "3.4.0"
-  lazy val catsCore               = "2.6.1"
+  lazy val catsCore               = "2.7.0"
   lazy val catsEffect             = "2.5.4"
   lazy val tofu                   = "0.10.1"
   lazy val derevo                 = "0.12.2"
